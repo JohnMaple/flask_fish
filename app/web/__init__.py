bd437c5c5997a5537e7614863aef054562ecf8e9
zxm <170893265@qq.com>
2019-04-08 16:40:28 +0800

    运行环境

--- app/web/__init__.py
+++ app/web/__init__.py
@@ -11,7 +11,7 @@ from app.web import book, auth, drift, gift, main, wish
 @web.app_errorhandler(404)
 def not_found(e):
     """
-    可以实现各种业务，如：把异常添加到日志里面
+    可以实现各种业务，如：把e异常里面的错误信息添加到日志里面
     基于aop思想（面向切片编程）
     :param e:
     :return:
